2caf36f367015353126cc832d700fa83a7a38df3
Nozel Rosario <nozelrosario@gmail.com>
2017-06-06 02:13:11 +0530

    using integrated express-pouchdb as default db

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const port = process.env.PORT || 5050;
 
 app.listen(port, function (err) {
     if (err) {
+        console.error("D-Care Server failed to start on on Port:" + port);
         throw err
     }    
     console.log("D-Care Server is listening on Port:" + port);
 });
+
+//DB
+var PouchDB = require('pouchdb');
+var PouchDBStore = PouchDB.defaults({
+    prefix: 'Databases/'
+});
+var PouchDBServer = require('express-pouchdb')({
+    mode: 'fullCouchDB',
+    configPath: 'Databases/config.json',
+    overrideMode: {
+        include: ['routes/fauxton']
+    }
+});
+PouchDBServer.setPouchDB(PouchDBStore);
+PouchDBServer.listen(5984);
