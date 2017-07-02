a9245103a968829920ced7b2b8c47e8d956e4cea
Nozel Rosario <nozelrosario@gmail.com>
2017-07-03 01:29:01 +0530

    Added conditional db run based on cmd args.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const app = require('./app');
 const appPort = process.env.PORT || 5050;
 const dbPort = 5984;
+var cmdLineArgs, PouchDB, PouchDBStore, PouchDBServer;
+//NR: Setup commandline arguments values with its defaults
+cmdLineArgs = {
+    //'argumentName': { 'value': 'defaultValue' },
+    'nodb': { 'value': false }
+};
+process.argv.forEach(function (val, index, array) {
+    //console.log(index + ': ' + val);
+    if (cmdLineArgs && cmdLineArgs[val]) {
+        cmdLineArgs[val].value = true;
+    }
+});
 
 app.listen(appPort, function (err) {
     if (err) {
@@ -11,22 +23,24 @@ app.listen(appPort, function (err) {
 });
 
 // DB Initialization
-var PouchDB = require('pouchdb');
-var PouchDBStore = PouchDB.defaults({
-    prefix: 'Databases/'
-});
-var PouchDBServer = require('express-pouchdb')({
-    mode: 'fullCouchDB',
-    configPath: 'Databases/config.json',
-    overrideMode: {
-        include: ['routes/fauxton']
-    }
-});
-PouchDBServer.setPouchDB(PouchDBStore);
-PouchDBServer.listen(dbPort, function (err) {
-    if (err) {
-        console.error("D-Care DB Server failed to start on on Port:" + dbPort);
-        throw err
-    }
-    console.log("D-Care DB Server is listening on Port:" + dbPort);
-});
+if (!cmdLineArgs.nodb.value) {
+    PouchDB = require('pouchdb');
+    PouchDBStore = PouchDB.defaults({
+        prefix: 'Databases/'
+    });
+    PouchDBServer = require('express-pouchdb')({
+        mode: 'fullCouchDB',
+        configPath: 'Databases/config.json',
+        overrideMode: {
+            include: ['routes/fauxton']
+        }
+    });
+    PouchDBServer.setPouchDB(PouchDBStore);
+    PouchDBServer.listen(dbPort, function (err) {
+        if (err) {
+            console.error("D-Care DB Server failed to start on on Port:" + dbPort);
+            throw err
+        }
+        console.log("D-Care DB Server is listening on Port:" + dbPort);
+    });
+}
